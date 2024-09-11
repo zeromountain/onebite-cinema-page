cb6982befc6d4d22667652de31a5d4f285d609e5
zeromountain <sonyeongsan@gmail.com>
2024-09-11 23:32:23 +0900

    mission 04: fix search form

--- src/components/common/search-form.tsx
+++ src/components/common/search-form.tsx
@@ -1,20 +1,41 @@
 import { useRouter } from "next/router";
+import { useEffect, useRef } from "react";
 
 export default function SearchForm() {
   const router = useRouter();
+  const { q } = router.query;
+
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const formData = new FormData(e.target as HTMLFormElement);
-    const searchQuery = formData.get("search");
+
+    const searchQuery = inputRef.current?.value;
+
+    if (!searchQuery) {
+      alert("검색어를 입력해 주세요.");
+      inputRef.current?.focus();
+      return;
+    }
+
     router.push(`/search?q=${searchQuery}`);
   };
+
+  useEffect(() => {
+    if (!q) {
+      if (inputRef.current) {
+        inputRef.current.value = "";
+      }
+    }
+  }, [q]);
 
   return (
     <form className="flex gap-2" onSubmit={handleSubmit}>
       <input
+        ref={inputRef}
         type="text"
         name="search"
+        defaultValue={q}
         placeholder="영화 제목을 입력해 주세요.."
         className="flex-1 p-2 rounded-md border-2 border-gray-300 bg-transparent outline-none focus:border-blue-500"
       />
