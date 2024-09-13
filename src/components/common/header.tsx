6a8cad01bafe7f0265d0405d0689b48b3806ffb2
zeromountain <sonyeongsan@gmail.com>
2024-09-13 12:43:40 +0900

    style: fix header style

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 h-20 flex items-center">
+    <header className="sticky top-0 h-20 flex items-center z-10 bg-black">
       <Link href="/">
         <h1 className="text-2xl font-bold text-red-500">🎥 ONEBITE CINEMA</h1>
       </Link>
